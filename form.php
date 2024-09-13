2177b52e429bd18b5a9d44abc9cbbd25b821fa11
bayangosclifford <bayangosclifford@gmail.com>
2024-09-13 15:45:46 +0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <form action="new.php" method= "POST" enctype="multipart/form-data">
+            Name: <input type = "text" name="name"><br>
+            Nickname: <input type = "text" name="nickName"><br>
+            Motto: <input type = "text" name="motto"><br>
+            LinkedIn Account: <input type = "text" name="linkedIn"><br>
+            Profile Image: <input type="file" name="image"><br>
+            <button>Submit</button>
+        </form>
+        
+        
+    </body>
+</html>
---
+++ form2.php
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <form action="new.php" method= "GET" enctype="multipart/form-data">
+            Name: <input type = "text" name="name"><br>
+            Nickname: <input type = "text" name="nickName"><br>
+            Motto: <input type = "text" name="motto"><br>
+            LinkedIn Account: <input type = "text" name="linkedIn"><br>
+            <button >Submit</button>
+        </form>
+        
+        
+    </body>
+</html>
